6d76a507dbe1e02cc781084bd2e1f527ba6d6f79
0abdullah0sami0 <0abdullah0sami0@gmail.com>
2023-05-07 22:47:34 +0300

    Updates on 07/05/2023

--- app.R
+++ app.R
@@ -35,7 +35,7 @@ ui <- dashboardPage(
                tabPanel("Service description",
                         column(width = 12,align = "center",
                                h3("Employee report"),
-                               p("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+                               p("This service is a gift from me for employees to help you track your progress in transactions processing in a simple and easy way using 7 indicators. To learn more about this tool, you can check the video below and download the sample data excel file to start using it yourself."),
                                br(),
                                fluidRow(downloadButton("id1","Download Excel file")
                                         ),
@@ -51,15 +51,28 @@ ui <- dashboardPage(
                tabPanel("Dashboard",
                         column(width = 12,align = "center",
                                h3("Upload File"),
-                               p("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+                               column(width = 12,align = "left",
+                                      p("The objective of this application is to generate 7 indicators using the employee's transactions history database in the form of a dashboard. The indicators included in the dashboard are:"),
+                                      tags$ol(
+                                        tags$li("Total number of transactions"),
+                                        tags$li("Average time period to complete transactions"),
+                                        tags$li("Most finished transaction type"),
+                                        tags$li("Total count of transactions by status"),
+                                        tags$li("Total count of transaction type by status"),
+                                        tags$li("Average process time period by transaction"),
+                                        tags$li("Count of finished transactions by date")
+                                        
+                                      )
+                                      ),
                                br(),
                                fluidRow(fileInput("id2", "",accept = ".xlsm")
                                         ),
                                br(),
                                fluidRow(
-                                 column(width = 2, valueBoxOutput("box1", 12)
-                                        ),
-                                 column(width = 2, valueBoxOutput("box2", 12)
+                                 column(width = 4,valueBoxOutput("box1", 12),
+                                        valueBoxOutput("box2", 12),
+                                        valueBoxOutput("box3", 12),
+                                        
                                         ),
                                  column(width = 4, plotlyOutput("plot1")
                                         ),
@@ -84,7 +97,7 @@ server <- function(input, output) {
     filename = function() {
       paste0("My data ", Sys.Date(), ".xlsm")},
     content = function(My_data){
-      file.copy("My data.xlsm",My_data)}
+      file.copy("database.xlsm",My_data)}
     )
   observeEvent(input$id2,{
     print(input$id2)
@@ -97,87 +110,102 @@ server <- function(input, output) {
     print(names(df()))
     output$plot1 <- renderPlotly({
       plot1 <- df() %>%
-        filter(!is.na(`situation‏`)) %>%
-        group_by(`situation‏`) %>%
+        filter(!is.na(situation)) %>%
+        group_by(situation) %>%
         summarise(count = n()) %>%
-        ggplot(aes(x = `situation‏`, y = count, fill = `situation‏`)) +
-                    geom_bar(stat = "identity") +
-                    theme_classic() +
-                    theme(legend.position = "none", axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=1), plot.title = element_text(hjust = 0.5)) +
-                    ggtitle("Total count by situation") +
-                    xlab("") +
-                    ylab("") +
-                    scale_fill_brewer(palette="Dark2")
-                  
-                   ggplotly(plot1,tooltip = c("y"))
+        ggplot(aes(x = situation, y = count, fill = situation)) +
+        geom_bar(stat = "identity") +
+        theme_classic() +
+        theme(legend.position = "none", axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=1), plot.title = element_text(hjust = 0.5)) +
+        ggtitle("Total count of transactions by status") +
+        xlab("") +
+        ylab("") +
+        scale_fill_manual(values = c("#BFBFBF", "#417FA6"))
+        # scale_fill_brewer(palette="Dark2")
+      ggplotly(plot1,tooltip = c("y")
+               )
                 })
-                
-                output$plot2 <- renderPlotly({
-                plot2 <- df() %>%
-                    filter(!is.na(`situation‏`)) %>%
-                    group_by(`type of process`,`situation‏`) %>%
-                    summarise(count = n()) %>%
-                    
-                    ggplot(aes(x = `type of process`, y = count, fill = `situation‏`)) +
-                    geom_bar(stat = "identity") +
-                    ggtitle("Count of transactions by situation") +
-                    xlab("") +
-                    ylab("") +
-                    theme_classic() +
-                    theme(legend.position = "none", axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=1), plot.title = element_text(hjust = 0.5)) +
-                    scale_fill_brewer(palette="Dark2")
-                
-                    ggplotly(plot2,tooltip = c("y","fill"))
-                  
-                })
-                
-                output$plot3 <- renderPlotly({
-                  plot3 <- df() %>%
-                           filter(`situation‏` == "finished") %>%
-                           mutate(Avrage = `last process` - `first process`) %>%
-                           group_by(`type of process`) %>%
-                           summarise(Avrage = mean(Avrage)) %>%
-                           
-                           ggplot(aes(x = `type of process`, y = Avrage, fill = `type of process`)) +
-                           geom_bar(stat = "identity") +
-                           ggtitle("Average process time by transaction") +
-                           xlab("") +
-                           ylab("") +
-                           theme_classic() +
-                           theme(legend.position = "none", axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=1), plot.title = element_text(hjust = 0.5)) +
-                           scale_fill_brewer(palette="Dark2")
-                          
-                  ggplotly(plot3,tooltip = c("y"))
-                })
-                
-                output$plot4 <- renderPlotly({
-                plot4 <- df() %>%
-                         filter(`situation‏` == "finished") %>%
-                         group_by(`last process`) %>%
-                         summarise(count = n()) %>%
-                  
-                  ggplot(aes(x = `last process`, y = count)) +
-                  geom_line(color = "#1C9E77") +
-                  ggtitle("Number of finished transactions by date") +
-                  xlab("End of transaction") +
-                  ylab("") +
-                  theme_classic() +
-                  theme(legend.position = "none", axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=1), plot.title = element_text(hjust = 0.5))
-                  
-                
-                ggplotly(plot4,tooltip = c("y"))
-                  
-                })
-                
-                output$box1 <- renderValueBox({
-                  boxA = df() %>%
-                    filter(!is.na(`type of process`)) %>%
-                    summarise(count = n())
-                  
-                  valueBox(boxA$count,"Number of transactions")
-                  
-                })
-})}
+    output$plot2 <- renderPlotly({
+      plot2 <- df() %>%
+        filter(!is.na(situation)) %>%
+        group_by(`type of process`,situation) %>%
+        summarise(count = n()) %>%
+        ggplot(aes(x = `type of process`, y = count, fill = situation)) +
+        geom_bar(stat = "identity") +
+        ggtitle("Count of each transaction type by status") +
+        xlab("") +
+        ylab("") +
+        theme_classic() +
+        theme(legend.position = "none", axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=1), plot.title = element_text(hjust = 0.5)) +
+        scale_fill_manual(values = c("#BFBFBF", "#417FA6"))
+        # scale_fill_brewer(palette="Dark2")
+      ggplotly(plot2,tooltip = c("y","fill")
+               )
+      })
+    output$plot3 <- renderPlotly({
+      plot3 <- df() %>%
+        filter(situation == "finished") %>%
+        mutate(`last process` = as.Date(`last process`,format = "%y-%m-%d"),
+               `first process` = as.Date(`first process`,format = "%y-%m-%d"),
+               Average = `last process` - `first process`) %>%
+        group_by(`type of process`) %>%
+        summarise(Average = round(mean(Average))) %>%
+        ggplot(aes(x = `type of process`, y = Average, fill = `type of process`)) +
+        geom_bar(stat = "identity") +
+        ggtitle("Average process time by transaction in days") +
+        xlab("") +
+        ylab("Days") +
+        theme_classic() +
+        theme(legend.position = "none", axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=1), plot.title = element_text(hjust = 0.5)) +
+        scale_fill_manual(values = c("#A2A6F2","#BFBFBF", "#417FA6","#3F89A6","#0D0D0D"))
+      ggplotly(plot3,tooltip = c("y")
+               )
+      })
+    output$plot4 <- renderPlotly({
+      plot4 <- df() %>%
+        filter(situation == "finished") %>%
+        mutate(`last process` = as.Date(`last process`,format = "%y-%m-%d")) %>%
+        group_by(`last process`) %>%
+        summarise(count = n()) %>%
+        ggplot(aes(x = `last process`, y = count)) +
+        geom_line(color = "#3F89A6") +
+        geom_point() +
+        ggtitle("Count of finished transactions by date") +
+        xlab("End of transaction") +
+        ylab("") +
+        theme_classic() +
+        theme(legend.position = "none", axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=1), plot.title = element_text(hjust = 0.5))
+      ggplotly(plot4,tooltip = c("y")
+               )
+      })
+    output$box1 <- renderValueBox({
+      boxA = df() %>%
+        filter(!is.na(`type of process`)) %>%
+        summarise(count = n()
+                  )
+      valueBox(boxA$count,"Number of transactions")
+      })
+    output$box2 <- renderValueBox({
+      boxB = df() %>%
+        filter(situation == "finished") %>%
+        mutate(`last process` = as.Date(`last process`,format = "%y-%m-%d"),
+               `first process` = as.Date(`first process`,format = "%y-%m-%d"),
+               Average = `last process` - `first process`) %>%
+        summarise(Average = round(mean(Average)))
+      valueBox(boxB$Average,"Average process time in days")
+    })
+    output$box3 <- renderValueBox({
+      boxC = df() %>%
+        filter(situation == "finished") %>%
+        group_by(`type of process`) %>%
+        summarise(count = n())
+      
+      boxC <- boxC[boxC$count == max(boxC$count),"type of process"]
+      
+      valueBox(boxC$`type of process`,"Most finished transaction type")
+    })
+    })
+  }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
